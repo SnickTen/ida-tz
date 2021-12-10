bb5187c2a0065d55aec2eeb787136fcb9484f279
SnickTen <i.bakunin@mail.ru>
2021-12-11 00:47:13 +0300

    Start Project

---
+++ components/sections/main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'SectionMain'
+}
+</script>
+
+<style lang="scss" module>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section-main />
+</template>
+
+<script>
+import SectionMain from '../components/sections/main'
+export default {
+  components: {
+    SectionMain
+  }
+}
+</script>
